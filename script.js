dddeeae8724c4b71767fead00d5cfd52fdbe3926
eiikook <exequelteodoroloresto28@gmail.com>
2023-12-05 20:31:14 +0800

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa yeeeeeeyyy";
+  question.innerHTML = "yeeeeeeyyy";
   gif.src =
     "https://raw.githubusercontent.com/eiikook/img/main/gif.webp";
 });
